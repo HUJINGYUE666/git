b88ef74955f17650de00b18a632f914767d345ae
HUJINGYUE666 <591711447@qq.com>
2019-03-16 09:13:22 +0800

    text

---
+++ game/1-贪吃蛇/dome.js
@@ -0,0 +1,12 @@
+//点击开始游戏 starPage消失 游戏开始
+//随机出现实物 出现三节蛇开始运动
+//上下左右 根据方向 改变方向运动
+//判断是否吃到食物 食物消失 蛇加1
+//判断游戏结束 弹出框
+
+var oContent = document.getElementById('content');
+var oTarPage = document.getElementById('starPage');
+
+
+
+
