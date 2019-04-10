1d54c5a4d8002fa0204f848de0cb08ed5be11f1e
胡竞月 <591711447@qq.com>
2019-04-10 15:07:27 +0800

    test

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,1 +1,4 @@
 console.log('hello');
+document.write('pink');
+import "./index.css";
+import sass from './css/common.scss';
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,11 +1,58 @@
 const path = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
-    mode:'development',
+    mode:'development',//production上线 js文件压缩
     entry:{
         index:'./src/index.js',
     },
     output:{
-        path:path.resolve(__dirname,'dist'),
-        filename:'index.js'
+        path:path.resolve(__dirname,'dist'), 
+        filename:'[name].js'
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                // use:['style-loader','css-loader']
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                  })
+            },{
+                test:/\.(jpg|png|git)/,
+                use:[{
+                    loader:'url-loader',
+                    options:{
+                        limit:500,
+                        outputPath:'images/'
+                    }
+                }]
+            },{
+                test:/\.(htm|html)/i,
+                use:['html-withimg-loader']
+            },{
+                test:/\.scss$/,
+                use:['style-loader','css-loader','sass-loaer']
+            }
+        ]
+    },
+    plugins:[
+        new HtmlPlugin({
+            //压缩 
+            minify:{
+                removeAttributeQuotes:true
+            },
+            template:'./src/index.html',
+            hash:true,
+        }),
+        new ExtractTextPlugin("index.css"),
+    ],
+    devServer:{
+        contentBase:path.resolve(__dirname,'dist'),
+        host:'localhost',
+        compress:true,
+        port:8081,
+        open:true
     }
 }
