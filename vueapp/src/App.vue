40d236c4f2d9e8bc4078219901545068c088dea8
hujingyue <591711447@qq.com>
2019-04-27 10:26:06 +0800

    test

---
+++ vueapp/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <common-header></common-header>
+  </div>
+</template>
+<script>
+  import CommonHeader from '@/components/CommonHeader.vue';
+  export default {
+      components: {
+        CommonHeader
+      }
+  }
+</script>
+<style>
+ 
+</style>
