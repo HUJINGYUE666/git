10a0e529e508236241794dbbfead5ba80b7becad
hujingyue <591711447@qq.com>
2019-05-15 15:04:45 +0800

    test

--- 移动端/海报/index.js
+++ 移动端/海报/index.js
@@ -1,8 +1,8 @@
 var iNow = 1;
 $(document).on('swipeUp',function(){
-    // 当前图片 滑上去   添加class "page-1 moveToTop"
+    // 当前图片滑上去  添加class"page-1 moveToTop"
     $('.page-'+iNow).attr("class","page-"+iNow+" moveToTop");
-    // 找到下一张图 从下边滑出来   "page-2 moveFromBottom"(iNow+1)
+    // 下一张图从下滑出来
     $('.page-'+(iNow+1)).attr("class","page-"+(iNow+1)+" moveFromBottom");
     setTimeout(function(){
         $('.page-'+(iNow+1)).children().removeClass('hide');
@@ -11,9 +11,9 @@ $(document).on('swipeUp',function(){
     }, 600);
 });
 $(document).on('swipeDown',function(){
-    // 当前图片 滑下去   
+    // 当前图片滑下
     $('.page-'+iNow).attr('class','page-'+iNow+" moveToBottom");
-    // 上一张图 滑下去    
+    // 上一张图滑下  
     $('.page-'+(iNow-1)).attr("class","page-"+(iNow-1)+" moveFromTop");
     setTimeout(function(){
         $('.page-'+(iNow-1)).children().removeClass('hide');
