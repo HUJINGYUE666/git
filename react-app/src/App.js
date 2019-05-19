8b352c8aee1ce6545327fc98cc5161ff76333b0b
hujingyue <591711447@qq.com>
2019-05-19 11:47:40 +0800

    test

---
+++ react-app/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+// import {BrowserRouter as Router, Route, Link } from "react-router-dom";
+import {BrowserRouter as Router, Route, Link , Switch } from "react-router-dom";
+import Home from './components/Home'
+import A from './components/A'
+import B from './components/B'
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Link to='/'>首页</Link>
+        ----
+        <Link to='/a'>A页面</Link>
+        ----
+        <Link to='/b'>B页面</Link>
+        {/* <Route exact path='/' component={Home}></Route>
+        <Route path='/a' component={A}></Route>
+        <Route path='/b' component={B}></Route> */}
+        <Switch>
+          <Route path='/a' component={A}></Route>
+          <Route path='/b' component={B}></Route>
+          <Route path='/' component={Home}></Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-app/src/components/A.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+// rcc
+export default class A extends Component {
+  render() {
+    return (
+      <div>
+        A页面content
+      </div>
+    )
+  }
+}
